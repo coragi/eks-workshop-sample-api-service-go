94da5c294aefc0c505c239adaf9c0207c8624804
Danilo Silveira <coragi@gmail.com>
2021-07-12 16:32:24 -0300

    atualizacao para Octank

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Olá Octank! Estamos em SA-EAST-1 - Brasil!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
